da276b93476d587a036e5101f85fe3ff8d3395d6
Muntasir Abdullah <muntasir.abdullah01@gmail.com>
2021-03-15 20:43:26 +0600

    Add TestNG, Create TestNg Class

---
+++ src/test/java/com/TestNG/TestNgDemo.java
@@ -0,0 +1,17 @@
+package com.TestNG;
+
+import org.testng.annotations.Test;
+
+public class TestNgDemo {
+
+	@Test
+	public void Demo2() {
+		System.out.println("Demo2 Method Executed");
+	}
+
+	@Test
+	public void demo1() {
+		System.out.println("Demo Method Executed");
+	}
+
+}
